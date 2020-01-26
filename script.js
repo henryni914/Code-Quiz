eceb49b9a218dffd860033dd771f551d58ff6454
Henry Ni <henryni914@gmail.com>
2020-01-26 17:11:03 -0500

    added the function userClick which after one of the option buttons are clicked, the textContent is changed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var initialCountdown = 100;
 var buttonEl1 = document.getElementById("firstOption");
 var buttonEl2 = document.getElementById("secondOption");
 var buttonEl3 = document.getElementById("thirdOption");
-var buttonEl4 = document.getElementById("fourthtOption");
+var buttonEl4 = document.getElementById("fourthOption");
 var questionPool = {
     "questions" : ["A","B","C","D"],
     "option1" : ["Wow","Hello"],
@@ -13,7 +13,7 @@ var questionPool = {
     "option3" : ["Is","Name"],
     "option4" : ["Dead","is Henry"],
 }
-var i = 1;
+var i = 0;
 
 function startQuiz() {
     
@@ -22,16 +22,35 @@ function startQuiz() {
         quizTimer.textContent = initialCountdown; + "seconds left"
         headerEl.style.display = "none";
         startEl.style.display = "none";
-        document.getElementById("question").textContent = questionPool.questions[0];
-        document.getElementById("firstOption").textContent = questionPool.option1[0];
-        document.getElementById("secondOption").textContent = questionPool.option2[0];
-        document.getElementById("thirdOption").textContent = questionPool.option3[0];
-        document.getElementById("fourthOption").textContent = questionPool.option4[0];
+        document.getElementById("question").textContent = questionPool.questions[i];
+        document.getElementById("firstOption").textContent = questionPool.option1[i];
+        document.getElementById("secondOption").textContent = questionPool.option2[i];
+        document.getElementById("thirdOption").textContent = questionPool.option3[i];
+        document.getElementById("fourthOption").textContent = questionPool.option4[i];
     }, 1000);
 }
 
 
 //Create a function so when user clicks one of the options this function should run and display the next question and the possible options
 
+function userClick() {
+    i++;
+    document.getElementById("question").textContent = questionPool.questions[i];
+    document.getElementById("firstOption").textContent = questionPool.option1[i];
+    document.getElementById("secondOption").textContent = questionPool.option2[i];
+    document.getElementById("thirdOption").textContent = questionPool.option3[i];
+    document.getElementById("fourthOption").textContent = questionPool.option4[i];
+    console.log(i);
+//     document.write("This is working");
+}
+
+
 
 startEl.onclick = startQuiz;
+buttonEl1.onclick = userClick;
+buttonEl2.onclick = userClick;
+buttonEl3.onclick = userClick;
+buttonEl4.onclick = userClick;
+
+
+
