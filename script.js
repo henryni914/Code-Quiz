6e9a10c366f97f73ea4cabcf493bf47fa6c7ef97
Henry Ni <henryni914@gmail.com>
2020-01-26 16:31:36 -0500

    added text content into buttons and question header when clicking the start button

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ var headerEl = document.getElementById("header");
 var quizTimer = document.getElementById("timer");
 var startEl = document.getElementById("start");
 var initialCountdown = 100;
-var containerEl = document.getElementsByClassName("container");
+var buttonEl1 = document.getElementById("firstOption");
+var buttonEl2 = document.getElementById("secondOption");
+var buttonEl3 = document.getElementById("thirdOption");
+var buttonEl4 = document.getElementById("fourthtOption");
 var questionPool = {
-    "q1" : ["A","B","C","D"],
-    "q2" : ["A","B","C","D"],
+    "questions" : ["A","B","C","D"],
+    "option1" : ["Wow","Hello"],
+    "option2" : ["Kobe","My"],
+    "option3" : ["Is","Name"],
+    "option4" : ["Dead","is Henry"],
 }
-
-// The timer, question and answer options need to be hidden until the start button is clicked
-
+var i = 1;
 
 function startQuiz() {
     
@@ -18,8 +22,16 @@ function startQuiz() {
         quizTimer.textContent = initialCountdown; + "seconds left"
         headerEl.style.display = "none";
         startEl.style.display = "none";
-        // containerEl.style.display = "block";
+        document.getElementById("question").textContent = questionPool.questions[0];
+        document.getElementById("firstOption").textContent = questionPool.option1[0];
+        document.getElementById("secondOption").textContent = questionPool.option2[0];
+        document.getElementById("thirdOption").textContent = questionPool.option3[0];
+        document.getElementById("fourthOption").textContent = questionPool.option4[0];
     }, 1000);
 }
 
-document.addEventListener("click", startQuiz);
+
+//Create a function so when user clicks one of the options this function should run and display the next question and the possible options
+
+
+startEl.onclick = startQuiz;
