32695b0c321594828a1c2ba02d2da6229bd5ad33
Henry Ni <henryni914@gmail.com>
2020-01-27 17:50:07 -0500

    hide certain elements initially that become visible when clicking the start button and hidden again when a condition is met

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
 var headerEl = document.getElementById("header");
 var quizTimer = document.getElementById("timer");
 var startEl = document.getElementById("start");
+var questionEl = document.getElementById("question");
 var buttonEl1 = document.getElementById("firstOption");
 var buttonEl2 = document.getElementById("secondOption");
 var buttonEl3 = document.getElementById("thirdOption");
 var buttonEl4 = document.getElementById("fourthOption");
 var answerIs = document.getElementById("answerCheck");
 var questionPool = {
-    "questions" : [
+    "questions": [
         "What is 1 + 1?", //Question 0
-        "What's Kobe nickname?", 
+        "What's Kobe nickname?",
         "What does HTML stand for?"],
-    "option1" : [
+    "option1": [
         "2", //Question 0 Option 1
-        "KB-24", 
+        "KB-24",
         "HyperText Modern Language"],
-    "option2" : [
+    "option2": [
         "11", //Question 0 Option 2
-        "Purple Mamba", 
+        "Purple Mamba",
         "HyperText Musical Language"],
-    "option3" : [
+    "option3": [
         "undefined", //Question 0 Option 3
-        "Black Mamba", 
+        "Black Mamba",
         "Hmm...The...Mother...Language???"],
-    "option4" : [
+    "option4": [
         "0",  //Question 0 Option 4
-        "Kobe", 
+        "Kobe",
         "HyperText Markup Language"]
 }
 console.log(questionPool.questions.length);
-var answerKey = ["firstOption","thirdOption","fourthOption","fourthOption"];
+var answerKey = ["firstOption", "thirdOption", "fourthOption", "fourthOption"];
 var initialCountdown = 100;
 var i = 0;
 var j = 0;
 var score = 0;
 
+document.getElementById("answers").style.visibility = "hidden";
+document.getElementById("question").style.visibility = "hidden";
 
 function startQuiz() {
-    // if (i <= questionPool.length || initialCountdown == 0){
-    //     clearInterval(timerInterval);
-    //     document.getElementById("question").textContent = "Score: " + score;
-    //     buttonEl1.style.display = "none";
-    //     buttonEl2.style.display = "none";
-    //     buttonEl3.style.display = "none";
-    //     buttonEl4.style.display = "Start Over?";
-    // }else {
-    console.log(i);
-    var timerInterval = setInterval(function() {
-        if (i >= questionPool.questions.length || initialCountdown == 0){
+    var timerInterval = setInterval(function () {
+        if (i >= questionPool.questions.length || initialCountdown == 0) {
             clearInterval(timerInterval);
-        }else {
-        initialCountdown--;
-        quizTimer.textContent = initialCountdown + " seconds left"
-        headerEl.style.display = "none";
-        startEl.style.display = "none";
-        document.getElementById("question").textContent = questionPool.questions[i];
-        document.getElementById("firstOption").textContent = questionPool.option1[i];
-        document.getElementById("secondOption").textContent = questionPool.option2[i];
-        document.getElementById("thirdOption").textContent = questionPool.option3[i];
-        document.getElementById("fourthOption").textContent = questionPool.option4[i];
-    }}, 1000); //}
+            document.getElementById("question").textContent = "Score: " + score;
+            document.getElementById("answers").style.visibility = "hidden";
+            document.getElementById("question").style.visibility = "visible";
+            document.getElementById("firstOption").style.visibility = "visible";
+            document.getElementById("firstOption").textContent = "Start Over"
+        } else {
+            initialCountdown--;
+            quizTimer.textContent = initialCountdown + " seconds left"
+            headerEl.style.display = "none";
+            startEl.style.display = "none";
+            document.getElementById("answers").style.visibility = "visible";
+            document.getElementById("question").style.visibility = "visible";
+            document.getElementById("question").textContent = questionPool.questions[i];
+            document.getElementById("firstOption").textContent = questionPool.option1[i];
+            document.getElementById("secondOption").textContent = questionPool.option2[i];
+            document.getElementById("thirdOption").textContent = questionPool.option3[i];
+            document.getElementById("fourthOption").textContent = questionPool.option4[i];
+        }
+    }, 1000); //}
 }
 
 
@@ -74,38 +76,26 @@ function userClick() {
     document.getElementById("fourthOption").textContent = questionPool.option4[i];
     console.log(i);
     var target = event.target.id;
-    // console.log (target);
-    // if (i == 4 || initalCountdown == 0) {
-    //     document.getElementById("question").textContent = "Score: " + score;
-    //     clearInterval(timerInterval);
-    //     buttonEl1.style.display = "none";
-    //     buttonEl2.style.display = "none";
-    //     buttonEl3.style.display = "none";
-    //     buttonEl4.style.display = "Start Over?";
-        
-    // } else {
-            if (target == answerKey[j]) {
-                score = score + 10;
-                console.log(answerKey[j],"Your score is: " + score,target);
-                j++;
-                document.getElementById("answerCheck").textContent = "You are right!";
-                setTimeout(function(){
-                    document.getElementById("answerCheck").textContent = '';
-                }, 2000);
-            } else {
-                    initialCountdown = initialCountdown - 10;
-                    console.log(answerKey[j],initialCountdown);
-                    j++;
-                    document.getElementById("answerCheck").textContent = "You are wrong!";
-                    setTimeout(function(){
-                        document.getElementById("answerCheck").textContent = '';
-                    }, 2000);
-            }
-        
+    if (target == answerKey[j]) {
+        score = score + 10;
+        console.log(answerKey[j], "Your score is: " + score, target);
+        j++;
+        document.getElementById("answerCheck").textContent = "You are right!";
+        setTimeout(function () {
+            document.getElementById("answerCheck").textContent = '';
+        }, 2000);
+    } else {
+        initialCountdown = initialCountdown - 10;
+        console.log(answerKey[j], initialCountdown);
+        j++;
+        document.getElementById("answerCheck").textContent = "You are wrong!";
+        setTimeout(function () {
+            document.getElementById("answerCheck").textContent = '';
+        }, 2000);
+    }
+
     console.log(j);
 }
-
-
 
 
 startEl.onclick = startQuiz;
